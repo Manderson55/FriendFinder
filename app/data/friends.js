9c85ba919e403faded1bc063bba9db999950e16c
Manderson55 <mariela.z.anderson@gmail.com>
2017-08-07 18:07:07 -0600

    created json

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+// ===============================================================================
+// DATA
+// this array will hold the friends information
+
+var friendsArray = [
+  {
+    name: "",
+    photoURL: "",
+    answers: []
+  }
+];
+
+// Exporting the friendsArray to make it available to other files using require.
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,15 @@
+//make an array with the information stored in friends.js
+//from the survey results. 
+
+var possibleFriends = require('../data/friends.js');
+
+
 // Get all possible friends
-app.get("app/data/friends.js", function(req, res) {  //api call
-  res.json(friends);
+app.get("/api/friends.js", function(req, res) {  //api call
+  res.json(possibleFriends);
 });
 
-app.post("/postReservationData", function(req, res) {
+app.post("/api/friends", function(req, res) {
   // Look at the request body for the post information
   // Push this post information object to the 'friends' array
 });
